b6b88e7e182346bfc9fef47dc152154e83a294e0
Mikhail Sholeninov <medvedsholeninov@yandex.ru>
2020-11-24 15:31:27 +0300

    fix: Исправить замечания

--- index.js
+++ index.js
@@ -1,36 +1,35 @@
-const ClosebuttonNode = document.querySelector('.popup__close-button');
-const EditbuttonNode = document.querySelector('.profile__edit-button');
-const PopupNode = document.querySelector('.popup');
-
-EditbuttonNode.addEventListener('click', togglePopupVisibility);
-ClosebuttonNode.addEventListener('click', togglePopupVisibility);
+let closebuttonNode = document.querySelector('.popup__close-button');
+let editbuttonNode = document.querySelector('.profile__edit-button');
+let popupNode = document.querySelector('.popup');
+let popupFormNode = document.querySelector('.popup__form');
+let profileNameNode = document.querySelector('.profile__name');
+let profileInfoNode = document.querySelector('.profile__text');
+let submitButtonNode = document.querySelector('.popup__save-button');
+editbuttonNode.addEventListener('click', togglePopupVisibility);
+closebuttonNode.addEventListener('click', togglePopupVisibility);
 
 
 function togglePopupVisibility()
 {
-  PopupNode.classList.toggle('popup_visible');
-  PopupNode.classList.toggle('popup_hidden');
+  popupNode.classList.toggle('popup_hidden');
 }
 
 
 
-const PopupFormNode = document.querySelector('.popup__form');
-const ProfileNameNode = document.querySelector('.profile__name');
-const ProfileInfoNode = document.querySelector('.profile__text');
-const SubmitButtonNode = document.querySelector('.popup__save-button');
 
 
-SubmitButtonNode.addEventListener('click',Submit);
-PopupFormNode.addEventListener('submit', Submit);
+
+submitButtonNode.addEventListener('click',Submit);
+popupFormNode.addEventListener('submit', Submit);
 
 
 function Submit (event)
 {
   event.preventDefault();
-  const FormInputNameNode = document.querySelector('.popup__name-input');
-  const FormInputInfoNode = document.querySelector('.popup__info-input');
-  ProfileNameNode.textContent = FormInputNameNode.value;
-  ProfileInfoNode.textContent = FormInputInfoNode.value;
-  document.querySelector('.popup__name-input').setAttribute('placeholder',FormInputNameNode.value);
-  document.querySelector('.popup__info-input').setAttribute('placeholder',FormInputInfoNode.value);
+  const formInputNameNode = document.querySelector('.popup__name-input');
+  const formInputInfoNode = document.querySelector('.popup__info-input');
+  profileNameNode.textContent = formInputNameNode.value;
+  profileInfoNode.textContent = formInputInfoNode.value;
+  document.querySelector('.popup__name-input').setAttribute('placeholder',formInputNameNode.value);
+  document.querySelector('.popup__info-input').setAttribute('placeholder',formInputInfoNode.value);
 }
